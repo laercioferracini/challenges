8b20b689a9c731c150c99133cc6d24ffc978669a
Laercio Ferracini <you@example.com>
2019-11-16 10:29:19 -0300

    NEW: Resolving Strings challenges

---
+++ src/main/java/strings/JavaStringsIntroduction.java
@@ -0,0 +1,22 @@
+package strings;
+
+import java.util.Scanner;
+
+public class JavaStringsIntroduction {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        System.out.println(A.length() + B.length());
+        String a, b = "";
+        if (A.charAt(0) > B.charAt(0)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        a = String.valueOf(A.charAt(0)).toUpperCase().concat(A.substring(1, A.length() - 1));
+        b = String.valueOf(B.charAt(0)).toUpperCase().concat(B.substring(1, B.length() - 1));
+        System.out.println(a + " " + b);
+    }
+}
