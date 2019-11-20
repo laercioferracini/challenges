fa816342916b03e35e954b6c2ad22c49348b5644
Laercio Ferracini <you@example.com>
2019-11-20 13:34:17 -0300

    NEW: Resolving Pattern Syntax checker

---
+++ src/main/java/strings/PatternSyntaxChecker.java
@@ -0,0 +1,28 @@
+package strings;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ * @author lferracini
+ * @project = challenges
+ * @since <pre>20/11/2019</pre>
+ */
+public class PatternSyntaxChecker {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String pattern = in.nextLine();
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+        }
+
+    }
+}
